107fd3d638a3057d30f8f61c8bfed31222ea012e
Scott Henderson <scott.mc.henderson@gmail.com>
2025-04-22 21:05:41 -0700

    latest

--- tests/plasmaScore.test.js
+++ tests/plasmaScore.test.js
@@ -4,6 +4,6 @@ test('plasmaScore 70 & 4 to equal 1369', () => {
     expect(plasmaScore(70, 4)).toBe(1369);
   });
 
-  test('plasmaScore 100 & 10,000 to equal 25,502,500', () => {
-    expect(plasmaScore(100, 10000)).toBe(25502500);
-  });
+test('plasmaScore 100 & 10,000 to equal 25,502,500', () => {
+expect(plasmaScore(100, 10000)).toBe(25502500);
+});
